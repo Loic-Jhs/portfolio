8c853667b46a0b0e7538236b6909fb41b237353c
Loic Jouhans <loic.jouhans@yahoo.fr>
2021-05-12 12:59:59 +0200

    adding content in About page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,13 @@
 // == Import npm
 import React from 'react';
 import CV from 'src/assets/loic-jouhans-CV.pdf';
+import git from 'src/assets/img/git.png';
+import html from 'src/assets/img/html.png';
+import css from 'src/assets/img/css.png';
+import js from 'src/assets/img/javascript.png';
+import sql from 'src/assets/img/MySQL-Logo.png';
+import php from 'src/assets/img/php.png';
+import symfo from 'src/assets/img/Symfony.png';
 
 // == Import
 import './about.scss';
@@ -16,6 +23,18 @@ const About = () => (
         being passionate about this universe.
         I followed a 6 months course at the O'clock school to learn the basics of development.
         Oh, and if you want to see my CV you can download it below.
+      </div>
+      <div className="skills">
+        During my training, here are the different technologies I studied
+      </div>
+      <div className="icon">
+        <img className="git" src={git} alt="git logo" />
+        <img className="html" src={html} alt="html logo" />
+        <img className="css" src={css} alt="css logo" />
+        <img className="js" src={js} alt="javascript logo" />
+        <img className="php" src={php} alt="php logo" />
+        <img className="symfo" src={symfo} alt="symfony logo" />
+        <img className="sql" src={sql} alt="My sql logo" />
       </div>
     </main>
     <a href={CV} download className="face-button">
